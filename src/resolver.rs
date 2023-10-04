91d80bc8ba5af05037c738a0d2c1106644c55e02
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-10-04 23:37:31 +1100

    Format `resolver::Ty` using `fmt::Display`

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use indexmap::IndexMap;
 
 use crate::indexer;
@@ -157,14 +159,14 @@ impl Index {
 
 				s.push_str(&parameter.name);
 				s.push(' ');
-				pretty_print_ty(&parameter.ty, &mut s);
+				s.push_str(&parameter.ty.to_string());
 			}
 
 			s.push(')');
 
 			if let Some(return_ty) = &procedure.return_ty {
 				s.push(' ');
-				pretty_print_ty(return_ty, &mut s);
+				s.push_str(&return_ty.to_string())
 			}
 
 			s.push('\n');
@@ -185,7 +187,7 @@ impl Index {
 						s.push_str("\n\t");
 						s.push_str(&field.name);
 						s.push(' ');
-						pretty_print_ty(&field.ty, &mut s);
+						s.push_str(&field.ty.to_string());
 					}
 
 					s.push_str("\n}\n");
@@ -197,10 +199,12 @@ impl Index {
 	}
 }
 
-fn pretty_print_ty(ty: &Ty, s: &mut String) {
-	match ty {
-		Ty::Int => s.push_str("int"),
-		Ty::Named(n) => s.push_str(n),
+impl fmt::Display for Ty {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Ty::Int => write!(f, "int"),
+			Ty::Named(name) => write!(f, "{name}"),
+		}
 	}
 }
 
